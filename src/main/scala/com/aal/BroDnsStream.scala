041ffbf815fcaa2d58d16ec39808e502cec69ddf
Ainun abdullah <abdullah.ainun4@gmail.com>
2018-12-27 00:30:30 +0700

    ganti topic

--- src/main/scala/com/aal/BroDnsStream.scala
+++ src/main/scala/com/aal/BroDnsStream.scala
@@ -42,7 +42,7 @@ object BroStream extends StreamUtils {
   def main(args: Array[String]): Unit = {
     val kafkaUrl = "kafka:9092"
     val shemaRegistryURL = "http://localhost:8081"
-    val topic ="bro"
+    val topic ="brodns"
 
     val spark = getSparkSession(args)
     import spark.implicits._
