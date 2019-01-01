44ece91ff2e6514b4ff80d644f38365cd925b943
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-01-01 23:12:17 +0700

    bismillah

--- src/main/scala/com/aal/spark/utils/StreamUtils.scala
+++ src/main/scala/com/aal/spark/utils/StreamUtils.scala
@@ -15,10 +15,10 @@ private[spark] trait StreamUtils {
   }
 
   def getSparkSession(args: Array[String]): SparkSession = {
-    val uri: String = args.headOption.getOrElse("mongodb://localhost/bro.connlog")
+    val uri: String = args.headOption.getOrElse("mongodb://10.148.0.4/bro.connlog")
 
     val conf = new SparkConf()
-      .setMaster ("spark://localhost:7077")
+      .setMaster ("spark://10.148.0.4:7077")
       .setAppName("StreamProtocolCountToMongo")
       .set("spark.app.id", "StreamProtocolCountToMongo")
       .set("spark.mongodb.input.uri", uri)
