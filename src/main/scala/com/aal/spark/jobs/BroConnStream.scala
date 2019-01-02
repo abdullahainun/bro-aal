bd15f56ecc918bee6940ab332144e165a8b0d65b
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-01-02 15:13:59 +0700

    development

--- src/main/scala/com/aal/spark/jobs/BroConnStream.scala
+++ src/main/scala/com/aal/spark/jobs/BroConnStream.scala
@@ -132,6 +132,54 @@ object BroConnStream extends StreamUtils {
 
     parsedLogData.printSchema
 
+
+    val ip_normal_app_host: HashMap[String, String] = HashMap(
+        ("192.168.50.19", "dropbox" ),
+        ("192.168.50.50", "avast"),
+        ("192.168.50.51", "adobe_reader"),
+        ("192.168.50.52", "Adobe_Software_Suite"),
+        ("192.168.50.54", "Chrome"),
+        ("192.168.50.55", "Firefox"),
+        ("192.168.50.56", "Malwarebyte"),
+        ("192.168.50.57", "WPS_office"),
+        ("192.168.50.58", "Windows_update"),
+        ("192.168.50.59", "bittorent"),
+        ("192.168.50.60", "audacity"),
+        ("192.168.50.61", "Bytefence"),
+        ("192.168.50.63", "Thunderbird"),
+        ("192.168.50.64", "avast"),
+        ("192.168.50.65", "Skype"),
+        ("192.168.50.66", "Facebook_massager"),
+        ("192.168.50.67", "CCleaner"),
+        ("192.168.50.68", "Windows_update"),
+        ("192.168.50.69", "Hitmanpro")
+    )
+
+    val ip_botnet_app_host: HashMap[String, String] = HashMap(
+        ("192.168.50.14", "zyklon"),
+        ("192.168.50.15", "blue"),
+        ("192.168.50.16", "liphyra"),
+        ("192.168.50.17", "gaudox"),
+        ("192.168.50.18", "blackout"),
+        ("192.168.50.30", "citadel"),
+        ("192.168.50.31", "citadel"),
+        ("192.168.50.32", "black_energy"),
+        ("192.168.50.34", "zeus")
+    )
+
+    val labeling = udf((ip_src: String) => {
+        var label = ""
+        if((ip_botnet_app_host contains ip_src) == true){
+             label  = "malicious"
+        }else if((ip_normal_app_host contains ip_src) == true){
+            label  = "normal"
+        }else{
+            label  = "normal"
+        }
+
+        label
+    })  
+
     val parsedRawDf = parsedLogData
       .withColumn("ts",to_utc_timestamp(from_unixtime(col("ts")),"GMT").alias("ts").cast(StringType))
       
@@ -146,7 +194,7 @@ object BroConnStream extends StreamUtils {
       .withColumn("TBT", BroConnFeatureExtractionFormula.tbt(col("orig_ip_bytes").cast("int"), col("resp_ip_bytes").cast("int")))
       .withColumn("APL", BroConnFeatureExtractionFormula.apl(col("PX").cast("int"), col("orig_ip_bytes").cast("int"), col("resp_ip_bytes").cast("int")))
       .withColumn("PPS", BroConnFeatureExtractionFormula.pps(col("duration").cast("double"), col("orig_pkts").cast("int"), col("resp_pkts").cast("int")))
-      .withColumn("label", BroConnLabeling.labeling(col("ts").cast("string")))
+      .withColumn("label", labeling(col("id.orig_h").cast("string")))
     
     val connDf = newDF
       .map((r:Row) => ConnCountObj(r.getAs[String](0),
