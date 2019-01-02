7d54441464bf2918354cb424685cf249a2548434
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-01-02 22:08:50 +0700

    development

--- src/main/scala/com/aal/spark/jobs/SimpleSVM.scala
+++ src/main/scala/com/aal/spark/jobs/SimpleSVM.scala
@@ -1,8 +1,16 @@
 package com.aal.spark.jobs
 import com.aal.spark.utils._
 
+// svm package
+import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.util.MLUtils
+
 object SimpleSVM extends StreamUtils {
     def main(args: Array[String]): Unit = {
+        // Load training data in LIBSVM format.
+        val data = MLUtils.loadLibSVMFile(sc, "/home/aal/bro-aal/sample_libsvm_data.txt")
+
         println("hello")
     }
 }
