eb737599cb0946bec065808deafb0901359094b6
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-01-03 20:06:28 +0700

    dev

--- src/main/scala/com/aal/spark/jobs/FileLogStream.scala
+++ src/main/scala/com/aal/spark/jobs/FileLogStream.scala
@@ -11,7 +11,7 @@ object FileLogStream extends StreamUtils {
     def main(args: Array[String]): Unit = {
         
         val sparkSession = getSparkSession(args)
-        import spark.implicits._
+        import sparkSession.implicits._
 
         val schema = StructType(
             Array(StructField("transactionId", StringType),
