984ea929b4f95249e9ff50157f35b5fa4e489d2f
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-01-09 05:38:49 +0700

    dev

--- src/main/scala/com/aal/spark/jobs/SimpleDT.scala
+++ src/main/scala/com/aal/spark/jobs/SimpleDT.scala
@@ -15,34 +15,10 @@ object SimpleDT extends StreamUtils {
         // $example on$
         // Load and parse the data file.
         val data = MLUtils.loadLibSVMFile(sc, "hdfs://10.252.108.22:9000/user/hduser/ainun/dataset_isot.data")
-        // Split the data into training and test sets (30% held out for testing)
-        val splits = data.randomSplit(Array(0.7, 0.3))
-        val (trainingData, testData) = (splits(0), splits(1))
-
-        // Train a DecisionTree model.
-        //  Empty categoricalFeaturesInfo indicates all features are continuous.
-        val numClasses = 2
-        val categoricalFeaturesInfo = Map[Int, Int]()
-        val impurity = "gini"
-        val maxDepth = 5
-        val maxBins = 32
-
-        val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
-        impurity, maxDepth, maxBins)
-
-        // Evaluate model on test instances and compute test error
-        val labelAndPreds = testData.map { point =>
-        val prediction = model.predict(point.features)
-        (point.label, prediction)
-        }
-        val testErr = labelAndPreds.filter(r => r._1 != r._2).count().toDouble / testData.count()
-        println(s"Test Error = $testErr")
-        println(s"Learned classification tree model:\n ${model.toDebugString}")
-
-        // Save and load model
-        model.save(sc, "hdfs://10.252.108.22:9000/user/hduser/ainun/tmp/myDecisionTreeClassificationModel")
-        val sameModel = DecisionTreeModel.load(sc, "hdfs://10.252.108.22:9000/user/hduser/ainun/tmp/myDecisionTreeClassificationModel")
-        // $example off$
+        val labelIndexer = new StringIndexer()
+        .setInputCol("label2")
+        .setOutputCol("indexedLabel")
+        .fit(data)
 
         sc.stop()
     }
--- src/main/scala/com/aal/spark/jobs/StreamAnalysis.scala
+++ src/main/scala/com/aal/spark/jobs/StreamAnalysis.scala
@@ -8,12 +8,43 @@ import org.apache.spark.mllib.tree.model.DecisionTreeModel
 import org.apache.spark.mllib.util.MLUtils
 // $example off$
 
+import org.apache.spark.ml.feature.{OneHotEncoderEstimator, VectorAssembler}
+import org.apache.spark.ml.classification.GBTClassifier
+
+import org.apache.spark.ml.feature.VectorSizeHint
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.sql.functions.col
+
 object StreamAnalysis extends StreamUtils {
     def main(args: Array[String]): Unit = {  
         val spark = getSparkSession(args)
 
         val data = spark.read.parquet("hdfs://10.252.108.22:9000/user/hduser/ainun/tmp/myDecisionTreeClassificationModel/data")
         data.printSchema()
+
+        val oneHot = new OneHotEncoderEstimator()
+        .setInputCols(Array("amountRange"))
+        .setOutputCols(Array("amountVect"))
+
+        val vectorAssembler = new VectorAssembler()
+        .setInputCols(Array("amountVect", "pcaVector"))
+        .setOutputCol("features")
+
+        val estimator = new GBTClassifier()
+        .setLabelCol("label")
+        .setFeaturesCol("features")
+
+        val vectorSizeHint = new VectorSizeHint()
+        .setInputCol("pcaVector")
+        .setSize(28)
+
+        val Array(train, test) = data.randomSplit(weights=Array(.8, .2))
+
+        val pipeline = new Pipeline()
+        .setStages(Array(oneHot, vectorSizeHint, vectorAssembler, estimator))
+
+        val pipelineModel = pipeline.fit(train)
 
         spark.stop()
     }
