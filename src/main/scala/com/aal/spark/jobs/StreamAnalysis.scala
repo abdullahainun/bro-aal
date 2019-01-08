b0a54756f7e06bebc6d678b2f34c5bdbc9891c24
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-01-08 22:14:25 +0700

    dev

---
+++ src/main/scala/com/aal/spark/jobs/StreamAnalysis.scala
@@ -0,0 +1,19 @@
+package com.aal.spark.jobs
+import com.aal.spark.utils._
+
+import org.apache.spark.{SparkConf, SparkContext}
+// $example on$
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.mllib.util.MLUtils
+// $example off$
+
+object SimpleDT extends StreamUtils {
+    def main(args: Array[String]): Unit = {  
+        val sc = getSparkSession(args)
+
+        val data = spark.read.parquet("hdfs://10.252.108.22:9000/user/hduser/ainun/tmp/myDecisionTreeClassificationModel")
+        data.printSchema()
+
+        sc.stop()
+    }
