0aa38c96e775ec9d815c82527b1e4c2dd86bb1b7
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-03-15 03:22:19 +0700

    general : debugging

--- src/main/scala/com/aal/spark/jobs/BroConnStream.scala
+++ src/main/scala/com/aal/spark/jobs/BroConnStream.scala
@@ -261,9 +261,9 @@ object BroConnStream extends StreamUtils {
         r.getAs[Integer](16),
         r.getAs[Integer](17),
         r.getAs[Integer](18),
-        r.getAs[Integer](19),
-        r.getAs[Integer](20),
-        r.getAs[Integer](21)
+        r.getAs[Integer](19)
+        // r.getAs[Integer](20),
+        // r.getAs[Integer](21)
         // r.getAs[Integer](22)
     //     r.getAs[Integer](25),
     //     r.getAs[Integer](26),
