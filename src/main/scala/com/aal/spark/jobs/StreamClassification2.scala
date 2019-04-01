9a8cbec306658202cbf23e8afef627517869ec0a
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-04-01 11:51:46 +0700

    general : debugging

--- src/main/scala/com/aal/spark/jobs/StreamClassification2.scala
+++ src/main/scala/com/aal/spark/jobs/StreamClassification2.scala
@@ -263,7 +263,7 @@ object StreamClassification2 extends StreamUtils {
     testing.printSchema()
     val testing2 = testing
                     .select(
-                      col("ts"),
+                      col("timestamp"),
                       col("uid"),
                       col("idOrigH"),
                       col("idOrigP"),
